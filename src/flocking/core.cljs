2bcdd36077a56154b4d3af7399d739c30e859798
JonyEpsilon <github@j-star.org>
2015-03-31 20:56:58 +0100

    Basic flocking algo.

--- src/flocking/core.cljs
+++ src/flocking/core.cljs
@@ -1,5 +1,5 @@
 (ns ^:figwheel-always flocking.core
-    (:require [reagent.core :as rc]))
+  (:require [reagent.core :as rc]))
 
 (enable-console-print!)
 
@@ -24,6 +24,54 @@
   [:polyline {:points points :stroke "red" :fill "none"}]
   )
 
-(defn render-flock [boids]
-  [:svg {:height 480 :width 640}
-   [:polyline {:points }]])
+
+(defn flock-debug []
+  [:ul
+   (map (fn [b] [:li (str b)]) @boids)])
+
+(defn mount-debug []
+  (rc/render-component [flock-debug]
+                       (.-body js/document)))
+
+(def cm-coeff 100.0)
+(def avoid-coeff -50.0)
+
+(defn centre-of-mass
+  [boids]
+  (let [n (count boids)
+        sx (reduce + (mapv :x boids))
+        sy (reduce + (mapv :y boids))]
+    {:x (/ sx n) :y (/ sy n)}))
+
+(defn boid-to-cm
+  [boid cm]
+  (let [dx (- (:x boid) (:x cm))
+        dy (- (:y boid) (:y cm))
+        newx (+ (:x boid) (/ dx cm-coeff))
+        newy (+ (:y boid) (/ dy cm-coeff))]
+    (assoc boid :x newx :y newy)))
+
+(defn avoid-a-random
+  [boids boid]
+  (let [other-boid (rand-nth boids)
+        dx (- (:x boid) (:x other-boid))
+        dy (- (:y boid) (:y other-boid))
+        newx (+ (:x boid) (/ dx avoid-coeff))
+        newy (+ (:y boid) (/ dy avoid-coeff))
+        ]
+    (assoc boid :x newx :y newy)))
+
+(defn update-boids
+  [boids]
+  (let [cm (centre-of-mass boids)
+        cm-boids (mapv (partial boid-to-cm cm) boids)
+        avoid-boids (mapv (partial avoid-a-random cm-boids) cm-boids)
+    ]
+    avoid-boids))
+
+(defn run []
+  (js/setInterval #(swap! boids update-boids) 1000))
+
+
+
+
