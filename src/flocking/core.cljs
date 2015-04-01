ab9001dc4a70e20f2cc4850f62799fc647bd2d79
Duncan Mortimer <dmortime@thoughtworks.com>
2015-04-01 08:42:06 +0100

    DM - Playing around with the simulation code

--- src/flocking/core.cljs
+++ src/flocking/core.cljs
@@ -3,9 +3,12 @@
 
 (enable-console-print!)
 
+;; Initialise
+
 (def ranges {:x 640 :y 480 :vx [-10 10] :vy [-10 10]})
 
 (defonce boids (rc/atom nil))
+(defonce config (rc/atom {:ps 1 :vs 10}))
 
 (defn rand-int-between [[l h]]
   (+ (rand-int (- h l)) l))
@@ -20,9 +23,10 @@
         vy (rand-int-between (:vy ranges))]
     (boid x y vx vy)))
 
-(defn init-boids [boid-atom]
-  (reset! boid-atom (doall (take 100 (repeatedly random-boid)))))
-  
+(defn init-boids [n boid-atom]
+  (reset! boid-atom (doall (take n (repeatedly random-boid)))))
+
+;; some debugging stuff
 (defn flock-debug []
   [:ul
    (map (fn [b] [:li (str b)]) @boids)])
@@ -31,8 +35,11 @@
   (rc/render-component [flock-debug]
                        (.-body js/document)))
 
-(def cm-coeff 0.001)
-(def avoid-coeff -1.0)
+;; simulation
+(def cm-coeff -0.9)
+(def avoid-coeff 0.5)
+(def damping-coeff 0.1)
+(def force-coupling 0.1)
 
 (defn centre-of-mass
   [boids]
@@ -42,38 +49,63 @@
     {:x (/ sx n) :y (/ sy n)}))
 
 (defn boid-to-cm
-  [boid cm]
+  [cm boid]
   (let [dx (- (:x boid) (:x cm))
         dy (- (:y boid) (:y cm))
-        newx (+ (:x boid) (* dx cm-coeff))
-        newy (+ (:y boid) (* dy cm-coeff))]
-    (assoc boid :x newx :y newy)))
+        s (Math/sqrt (+ (* dx dx) (* dy dy)))
+        ux (/ dx s)
+        uy (/ dy s)
+        fx (* ux cm-coeff)
+        fy (* uy cm-coeff)]
+    (assoc boid :fx fx :fy fy)))
 
 (defn avoid-a-random
   [boids boid]
-  (let [other-boid (rand-nth boids)
-        dx (- (:x boid) (:x other-boid))
-        dy (- (:y boid) (:y other-boid))
-        newx (+ (:x boid) (* dx avoid-coeff))
-        newy (+ (:y boid) (* dy avoid-coeff))
-        ]
-    (assoc boid :x newx :y newy)))
+  (let [other-boid (rand-nth boids)]
+    (if (and (= (:x boid) (:x other-boid))
+             (= (:y boid) (:y other-boid)))
+      boid
+      (let [dx (- (:x boid) (:x other-boid))
+            dy (- (:y boid) (:y other-boid))
+            s (Math/sqrt (+ (* dx dx) (* dy dy)))
+            ux (/ dx s)
+            uy (/ dy s)
+            fx (/ (* ux avoid-coeff) s)
+            fy (/ (* uy avoid-coeff) s)]
+        (-> boid
+            (update-in [:fx] (partial + fx))
+            (update-in [:fy] (partial + fy)))))))
+
+(defn damping [{:keys [vx vy] :as boid}]
+  (-> boid
+      (update-in [:fx] (partial + (* damping-coeff (- 0 vx))))
+      (update-in [:fy] (partial + (* damping-coeff (- 0 vy))))))
+
+(defn update-velocity [{:keys [vx vy fx fy] :as boid}]
+  (assoc boid
+         :vx (+ vx (* force-coupling fx))
+         :vy (+ vy (* force-coupling fy))))
+
+(defn update-position [{:keys [x y vx vy] :as boid}]
+  (assoc boid
+         :x (+ x vx)
+         :y (+ y vy)))
 
 (defn update-boids
   [boids]
   (let [cm (centre-of-mass boids)
-        _ (println cm)
-        cm-boids (mapv (partial boid-to-cm cm) boids)
-        avoid-boids (mapv (partial avoid-a-random cm-boids) cm-boids)
-    ]
-    avoid-boids))
+        boids' (->> boids
+                    (mapv (partial boid-to-cm cm))
+                    (mapv (partial avoid-a-random boids))
+                    (mapv damping)
+                    (mapv update-velocity)
+                    (mapv update-position))]
+    boids'))
 
 (defn run []
-  (js/setInterval #(swap! boids update-boids) 1000))
+  (js/setInterval #(swap! boids update-boids) 100))
 
-
-
-
+;; rendering
 (defn render-boid [coord-scale velocity-scale]
   (fn [{:keys [x y vx vy] :as boid}]
     (let [coords [[x y] [(+ x (* velocity-scale vx)) (+ y (* velocity-scale vy))]]
@@ -85,10 +117,11 @@
                       (apply str))]
       [:polyline {:points points :stroke "red" :fill "none"}])))
 
-(defn render-flock [boids-atom]
-  (let [boids @boids-atom]
+(defn render-flock [boids-atom config-atom]
+  (let [boids @boids-atom
+        {ps :ps vs :vs} @config-atom]
     [:svg {:height 480 :width 640}
-     (map #(vector (render-boid 1 0.5) %) boids)]))
+     (map #(vector (render-boid ps vs) %) boids)]))
 
 (defn list-render-flock [boids-atom]
   (let [boids @boids-atom]
@@ -96,7 +129,5 @@
      (map #(vector :li (str %)) boids)]))
 
 (defn mountit []
-  (rc/render-component [render-flock boids]
+  (rc/render-component [render-flock boids config]
                        (.-body js/document)))
-
->>>>>>> 89d5e1bda43301e34802b08e8462d0e4fc1b99de
